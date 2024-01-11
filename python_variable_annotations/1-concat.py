7e3bfd089a86167bd8a59e3d9906663ebce98f55
Mugisha-Beline <belinemugisha21@gmail.com>
2024-01-11 14:26:19 +0200

    validate

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -2,8 +2,8 @@
 
 """ Basic annotations concat """
 
+# Add two blank lines here
+
 def concat(str1: str, str2: str) -> str:
     """ Returns sum of two strings """
     return str1 + str2
-
-# Add a newline at the end of the file
